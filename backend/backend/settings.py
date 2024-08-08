192ad6fab76a4285acd959c59365cf1e19e6a96f
Maya Harvey <mayaharvey@mayas-mbp-5.lan>
2024-08-08 16:56:20 -0700

    misspelled postgres

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        "ENGINE": "django.db.backends.postgressql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PWD"),
