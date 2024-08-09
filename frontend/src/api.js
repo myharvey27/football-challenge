54406dd3aba3f53eb1ca2cf7ec3b3db2a5885cc8
Maya Harvey <mayaharvey@mayas-mbp-5.lan>
2024-08-08 18:09:10 -0700

    testing env variables

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,7 @@ import axios from "axios"
 import { ACCESS_TOKEN } from "./constants"
 
 const api = axios.create( {
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: "https://football-challenge-backend-d58da5a932d9.herokuapp.com"
 })
 
 api.interceptors.request.use(
